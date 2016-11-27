3df00776a688914df0bb435bf362e369e9f1475a
liuzhuocpp <liuzhuocpp@qq.com>
2016-11-27 10:36:23 +0800

    Revert "add Duomeiti"
    This reverts commit e8057970b4513b30648577e1daad3391a6051972.

--- DuoMeiTi/WebContent/sql/create.sql
+++
@@ -1,8 +0,0 @@
-create database duomeiti  default  character set utf8;
-
-set global       interactive_timeout=31536000;
- show global variables like '%timeout';
- 
- grant all privileges on *.*  to root@'%'  identified by  'root';
- 
- 202.118.67.200:8888
